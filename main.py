e0d53104f44e22ce3cc583d18843e888b44a5f5f
Vilbat <vilmer.astrom@gmail.com>
2020-12-09 19:55:23 +0100

    Init

---
+++ main.py
@@ -0,0 +1,57 @@
+# This is a sample Python script.
+
+import os
+
+import cv2
+import numpy as np
+
+
+def create_line_drawing_image(img):
+    """
+    kernel = np.array([
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+        ], np.uint8)
+    """
+
+    kernel = np.ones((5, 5))
+
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # return img_gray
+    img_dilated = cv2.dilate(img_gray, kernel, iterations=1)
+    # return img_dilated
+    img_diff = cv2.absdiff(img_dilated, img_gray)
+    # print(img_diff)
+
+    ret, img_diff = cv2.threshold(img_diff, 20, 255, cv2.THRESH_BINARY)
+
+    erode_kernel = np.ones((2, 2))
+
+    img_diff = cv2.erode(img_diff, erode_kernel, iterations=1)
+    img_diff = cv2.dilate(img_diff, erode_kernel, iterations=1)
+
+    contour = 255 - img_diff
+
+    return contour
+    # return img_diff
+
+
+def convert_images(dir_from, dir_to):
+    for file_name in os.listdir(dir_from):
+        print(file_name)
+        if file_name.endswith('.jpg'):
+            print(file_name)
+            img = cv2.imread(os.path.join(dir_from, file_name))
+            img_contour = create_line_drawing_image(img)
+            cv2.imwrite(os.path.join(dir_to, file_name), img_contour)
+
+
+if __name__ == '__main__':
+    dirIMG = 'images/source'
+    dirDEST = 'images/output'
+    convert_images(dirIMG, dirDEST)
+
+
